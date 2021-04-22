7fefd2895699db0b83a60d7fc6e88645ad27df14
Pranjul120568 <pranjuldwivedi120568@gmail.com>
2021-04-22 11:05:07 +0530

    View Binding Java

--- app/src/main/java/com/pdinc/alljavthings/MainActivity.java
+++ app/src/main/java/com/pdinc/alljavthings/MainActivity.java
@@ -3,12 +3,32 @@ package com.pdinc.alljavthings;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn2=findViewById(R.id.btn2);
+        btn2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.d("TAG","Btn 2 is pressed");
+            }
+        });
+    }
+Button btn3;
+    @Override
+    public void onClick(View v) {
+        btn3=findViewById(R.id.btn3);
+        Log.d("TAG","Btn 3 is pressed");
+    }
+    public void onButtonClicked(View view) {
+        Log.d("TAG","Btn 1 is pressed");
     }
 }
