3fe1c7ee0f3379cc6f546c9d5116b06db0845665
Pranjul120568 <pranjuldwivedi120568@gmail.com>
2021-04-25 23:05:05 +0530

    Correct Switch

--- app/src/main/java/com/pdinc/alljavthings/MainActivity.java
+++ app/src/main/java/com/pdinc/alljavthings/MainActivity.java
@@ -3,33 +3,47 @@ package com.pdinc.alljavthings;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button btn2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn2=findViewById(R.id.btn2);
-        btn2.setOnClickListener(new View.OnClickListener(){
+        btn2 = findViewById(R.id.btn2);
+        btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("TAG","Btn 2 is pressed");
+                Log.d("TAG", "Btn 2 is pressed");
+                //NORMAL PERMISSION
+                ConnectivityManager cm=(ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+                NetworkInfo netInfo=cm.getActiveNetworkInfo();
+                boolean isConnected=netInfo!= null && netInfo.isConnected();
+                TextView tv1=findViewById(R.id.tv1);
+                tv1.setText(isConnected ? "Connected":"DisConnected");
             }
         });
+
+         void onButtonClicked() {
+            Intent i;
+            i=new Intent(this,MainActivity2.class);
+            startActivity(i);
+        }
     }
-Button btn3;
+    Button btn3;
     @Override
     public void onClick(View v) {
-        btn3=findViewById(R.id.btn3);
-        Log.d("TAG","Btn 3 is pressed");
+        btn3 = findViewById(R.id.btn3);
+        Log.d("TAG", "Btn 3 is pressed");
     }
-    public void onButtonClicked(View view) {
-        startActivity(new Intent(this, MainActivity2.class));
-    }
+
 }
--- app/src/main/java/com/pdinc/alljavthings/MainActivity2.java
+++ app/src/main/java/com/pdinc/alljavthings/MainActivity2.java
@@ -1,14 +1,51 @@
 package com.pdinc.alljavthings;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    Button btn=findViewById(R.id.dialBtn);
+    EditText  et1=findViewById(R.id.dialTv);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int permission= ContextCompat.checkSelfPermission(MainActivity2.this, Manifest.permission.CALL_PHONE);
+                if(permission== PackageManager.PERMISSION_GRANTED){
+                    callNumber();
+                }else{
+                    ActivityCompat.requestPermissions(
+                            MainActivity2.this,
+                            new String[]{
+                                    Manifest.permission.CALL_PHONE
+                            },
+                     121
+                    );
+                }
+            }
+        });
+
     }
+    void callNumber() {
+      String telNo=et1.getText().toString();
+      Uri uri =Uri.parse("tel:" + telNo);
+        Intent i=new Intent(Intent.ACTION_CALL);
+        startActivity(i);
+    }
+
 }
